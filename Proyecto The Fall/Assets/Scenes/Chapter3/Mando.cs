c19f033533894492598dca0e618a370c4b94855c
JargX <jargdeveloper@gmail.com>
2024-05-10 13:47:50 +0200

    Menu Done

---
+++ Proyecto The Fall/Assets/Scenes/Chapter3/Mando.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mando : MonoBehaviour
+{
+    private Vector3 offset;
+    private float zCoord;
+    public static int itemCount2 = 0; // Contador de objetos recogidos como variable estática
+    public string escenaDeCambio; // Variable para almacenar el nombre de la escena de cambio
+
+    void OnMouseDown()
+    {
+        zCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        offset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = zCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + offset;
+    }
+
+    void OnMouseUp()
+    {
+        if (transform.position.x >= -1.1 && transform.position.x <= 1.1 &&
+            transform.position.y >= -6 && transform.position.y <= -1.4)
+        {
+            itemCount2++; // Incrementa el contador de objetos recogidos
+            Destroy(gameObject); // Destruye el objeto
+
+            if (itemCount2 == 1) // Verifica si todos los objetos han sido recogidos
+            {
+                ChangeScene();
+            }
+        }
+    }
+
+    void ChangeScene()
+    {
+        // Carga la escena definida en el inspector
+        UnityEngine.SceneManagement.SceneManager.LoadScene(escenaDeCambio);
+    }
+}
